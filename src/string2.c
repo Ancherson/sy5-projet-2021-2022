bd8a15b207ad412c2e32a001245466b4ee18ef5a
cherion <julescherion@yahoo.fr>
2021-11-08 22:22:39 +0100

    add an #include

--- src/string2.c
+++ src/string2.c
@@ -1,4 +1,5 @@
 #include "string2.h"
+#include <unistd.h>
 
 int create_string(string *dest, int len, char *str) {
     dest->len = len;
@@ -9,6 +10,6 @@ int create_string(string *dest, int len, char *str) {
 int write_string(int fd, string str){
     if (write(fd, &str.len, sizeof(uint32_t)) < sizeof(uint32_t))  return 1;
 
-    if (write(fd, str.str, str.len) !=str.len ) return 1;
+    if (write(fd, str.str, str.len) != str.len) return 1;
     return 0;
 }
