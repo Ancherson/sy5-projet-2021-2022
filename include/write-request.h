b4d7fdd5f1ec2362c204255637f356dfa3773b96
cherion <julescherion@yahoo.fr>
2021-11-15 15:42:43 +0100

    creation de write_create

--- include/write-request.h
+++ include/write-request.h
@@ -3,9 +3,11 @@
 
 #include <unistd.h>
 #include <stdint.h>
+#include <string.h>
 
 #include "convert-uint.h"
 #include "timing-text-io.h"
+#include "commandline.h"
 
 int write_opcode(int fd, uint16_t opcode);
 int write_timing(int fd, char * minutes_str, char * hours_str, char * daysofweek_str);
--- src/write-request.c
+++ src/write-request.c
@@ -1,8 +1,9 @@
 #include "write-request.h"
 
+
 int write_opcode(int fd, uint16_t opcode){
     opcode = reverse_byte16(opcode);
-    if (write(fd, &opcode, sizeof(uint16_t)) < sizeof(uint16_t) return 1;
+    if (write(fd, &opcode, sizeof(uint16_t)) < sizeof(uint16_t)) return 1;
     return 0;
 }
 
@@ -17,9 +18,19 @@ int write_timing(int fd, char * minutes_str, char * hours_str, char * daysofweek
     
 int write_taskid(int fd, uint64_t taskid){
     taskid = reverse_byte64(taskid);
-    if (write(fd, &taskid, sizeof(uint64_t)) < sizeof (unit64_t)) return 1;
+    if (write(fd, &taskid, sizeof(uint64_t)) < sizeof (uint64_t)) return 1;
     return 0;
 }
 
-int write_create(int fd, uint16_t opcode, char * minutes_str, char * hours_str, char * daysofweek_str, int argc, char **argv)
-{return 1;}
+int write_create(int fd, uint16_t opcode, char * minutes_str, char * hours_str, char * daysofweek_str, int argc, char **argv){
+    if (write_opcode(fd,opcode) == 1) return 1;
+    if (write_timing(fd,minutes_str,hours_str,daysofweek_str) == 1) return 1;
+    commandline *cmd = malloc(sizeof(commandline));
+    string *tab = malloc(sizeof(string) * (argc));
+    for(int i = 1; i < argc; i++) {
+        create_string(tab + (i - 1), strlen(argv[i]), argv[i]);
+    }
+    if (create_commandline(cmd, argc - 1, tab) != 0) return 1;
+    if (write_commandline(fd,*cmd) == 1) return 1;
+    return 0;
+}
