a2eb658310497af2337f4e8039db5c1163d60821
Alexandre LEYMARIE <alexandre.j.leymarie@gmail.com>
2021-11-22 14:36:54 +0100

    Correction d'une erreur de signe

--- src/write-request.c
+++ src/write-request.c
@@ -9,7 +9,7 @@ int write_opcode(int fd, uint16_t opcode){
 
 int write_timing(int fd, char * minutes_str, char * hours_str, char * daysofweek_str){
     timing t;
-    if(timing_from_strings(&t, minutes_str, hours_str, daysofweek_str) == 1) {
+    if(timing_from_strings(&t, minutes_str, hours_str, daysofweek_str) == -1) {
         printf("Erreur timing_from_strings\n");
         return 1;
     }
