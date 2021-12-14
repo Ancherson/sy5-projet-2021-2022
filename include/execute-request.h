641e58245d01182b02bac17cb65c84a66737c70f
Yoan Rougeolle <yoyo031201@orange.fr>
2021-12-14 11:28:58 +0100

    Ajout de la fonction qui lit la requete create, cree la tache et ecrit la reponse

--- include/execute-request.h
+++ include/execute-request.h
@@ -3,10 +3,12 @@
 #include "task.h"
 #include <sys/stat.h>
 #include "write-request.h"
+#include "read-reply.h"
 #include <fcntl.h>
 #include <errno.h>
 
-void create(task t);
+void do_create(task t);
+int create(int fd, char *buf, task **t, int *len);
 int list(char *buf, task *t, int len);
 
 #endif // EXECUTE_REQUEST_H
